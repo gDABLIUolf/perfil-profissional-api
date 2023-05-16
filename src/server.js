eedc77076034d33636ca3d1b6497e8bf0111d68b
Gabriel Wolf <gwolf8764.gw.gw@gmail.com>
2023-05-15 23:22:04 -0300

    adicionando a camada middleware para autenticação

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const notificacaoRoute = require("./routes/NotificacaoRoute");
 const app = express();
 const db = require("mongoose");
 
-db.connect("mongodb+srv://gwolf:b0n9ytrgm1x22okX@cluster0.6s24grv.mongodb.net/?retryWrites=true&w=majority")
 
 app.use(express.json());
 app.use("/perfil", perfilRoute);
